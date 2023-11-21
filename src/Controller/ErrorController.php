2366dd130fb1b43809884c6ee17302b022a6e819
greciaD03 <grecia.deyanira.mtz2003@gmail.com>
2023-11-21 16:57:59 -0600

    Avance (yaml)

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -11,8 +11,12 @@ class ErrorController extends AbstractController
     #[Route('/error', name: 'app_error')]
     public function index(): Response
     {
-        return $this->render('error/index.html.twig', [
-            'controller_name' => 'ErrorController',
+        return $this->render('bundles/TwigBundle/Exception/error404.html.twig', [
+            //'controller_name' => 'ErrorController',
         ]);
     }
 }
+
+//https://stackoverflow.com/questions/64241732/implement-a-custom-error-controller-in-symfony-4-4
+//https://github.com/partkeepr/PartKeepr/issues/1008 
+//https://symfony.com/doc/current/service_container/import.html
